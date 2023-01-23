c1e0e5c08396bf6dce17f397a1f4a0adb5e16937
Gasak Ruslan <rusya.gasak@mail.ru>
2023-01-23 17:51:50 +0300

    Add files to git, site doesn't work normally

---
+++ activities/people_active/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+# Create your views here.
+
+
+class Index(View):
+    def get(self,requests,*args,**kwargs):
+        return HttpResponse("Hello world!")
+
+    def __str__(self):
+        return self
