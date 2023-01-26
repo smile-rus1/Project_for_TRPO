5023efe6df31f7dca8a305977069b37e2241a9b5
Gasak Ruslan <rusya.gasak@mail.ru>
2023-01-26 22:30:00 +0300

    перестроена модель добавлено поле slug в 2 модели

--- activities/people_active/models.py
+++ activities/people_active/models.py
@@ -5,17 +5,19 @@ from django.urls import reverse
 
 
 class Activity(models.Model):
-    title = models.CharField(max_length=30)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%M/%d/")# записывает в раздельные папки по классификаторам
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    group = models.ForeignKey("Groups", on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=30, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=200, unique=True, db_index=True, verbose_name="URL")
+    content = models.TextField(blank=True, verbose_name="Текст", null=True)
+    photo = models.ImageField(upload_to="photos/%Y/%M/%d/", verbose_name="Фото", null=True)# записывает в раздельные папки по классификаторам
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    update_date = models.DateTimeField(auto_now=True, verbose_name="дата изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    group = models.ForeignKey("Groups", on_delete=models.PROTECT, verbose_name="Группы")
 
     """
     Возвращает название статьи в админке и не только
     """
+
     def __str__(self):
         return self.title
 
@@ -26,13 +28,22 @@ class Activity(models.Model):
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_id": self.pk})
 
+    class Meta:
+        verbose_name_plural = "События"
+        ordering = ["-update_date", "create_date","title"]
+
 
 class Groups(models.Model):
-    name = models.CharField(max_length=30, db_index=True)
-    title = models.TextField(blank=True)
+    name = models.CharField(max_length=30, db_index=True, verbose_name="Название")
+    slug = models.SlugField(max_length=200, unique=True, db_index=True, verbose_name="URL")
+    title = models.TextField(blank=True, verbose_name="Текст")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse("show_group", kwargs={"group_id": self.pk})
+
+    class Meta:
+        verbose_name_plural = "Группы"
+        ordering = ["name"]
