e1ae63f4c73f701770d8ea3eb6a9df8284be6bea
Gasak Ruslan <rusya.gasak@mail.ru>
2023-02-28 15:24:46 +0300

    изменена модель Subscribe, было добавлено ограничение на ключ group_sub, в __str__ начало возвращать имя группы

--- activities/people_active/models.py
+++ activities/people_active/models.py
@@ -70,10 +70,10 @@ class DiscussionActive(models.Model):
 class Subscribe(models.Model):
     date = models.DateTimeField(auto_now_add=True, verbose_name="Дата подписки")
     name_user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
-    group_sub = models.ForeignKey("Groups", on_delete=models.CASCADE, verbose_name="Группы")
+    group_sub = models.ForeignKey("Groups", on_delete=models.CASCADE, verbose_name="Группы", unique=True)
 
     def __str__(self):
-        return self.date
+        return self.group_sub.name
 
     class Meta:
         verbose_name_plural = "Подписки на группы"
